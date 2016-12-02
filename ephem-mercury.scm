a0e4bf1391eec733746c02b353328b92368ecd38
Robert Altenburg <rca@qrpc.com>
2016-12-02 16:27:58 -0500

    added mercury

---
+++ ephem-mercury.scm
@@ -0,0 +1,117 @@
+;;; ephem-mercury
+;;; Chicken scheme wrapper for libnova
+;;; 
+;;; see: libnova.sourceforge.net
+;;;
+;;; Rob Altenburg - 2016
+;;; 
+
+;;; Module Definition {{{1
+(module ephem-mercury
+        ( mercury-sdiam mercury-rst mercury-helio-coords mercury-equ-coords
+          mercury-earth-dist mercury-solar-dist mercury-magnitude 
+          mercury-disk mercury-phase mercury-rect-helio)
+
+    (import chicken scheme foreign)
+
+;;; }}}
+
+;;; Headers {{{1 
+    (foreign-declare "#include <libnova/mercury.h>")
+    (foreign-declare "#include <libnova/ln_types.h>")
+    (define-external (callback (scheme-object obj)) scheme-object obj)
+
+;;; }}} 
+
+;;; mercury {{{1
+    (define mercury-sdiam 
+      (foreign-lambda double "ln_get_mercury_sdiam" double))
+
+    ;; returns #(rise set transit) in jd
+    (define mercury-rst
+        (foreign-safe-lambda* scheme-object ((double jd) (double lng) (double lat))
+                       "C_word lst = C_SCHEME_END_OF_LIST, *a;
+                       struct ln_lnlat_posn in = {.lat = lat, .lng = lng};
+                       struct ln_rst_time *out;
+                       out = malloc(sizeof(struct ln_rst_time));
+                       ln_get_mercury_rst(jd, &in, out);
+                       a = C_alloc(C_SIZEOF_LIST(3) + C_SIZEOF_FLONUM * 3);
+                       lst = C_vector(&a, 3, 
+                                        C_flonum(&a, out->rise),
+                                        C_flonum(&a, out->set),
+                                        C_flonum(&a, out->transit));
+                       C_return(callback(lst));"))
+
+    ;; returns #(L B R) 
+    (define mercury-helio-coords
+        (foreign-safe-lambda* scheme-object ((double jd))
+                       "C_word lst = C_SCHEME_END_OF_LIST, *a;
+                       struct ln_helio_posn *r;
+                       r = malloc(sizeof(struct ln_helio_posn));
+                       ln_get_mercury_helio_coords(jd, r);
+                       a = C_alloc(C_SIZEOF_LIST(3) + C_SIZEOF_FLONUM * 3);
+                       lst = C_vector(&a, 3, 
+                                        C_flonum(&a, r->L),
+                                        C_flonum(&a, r->B),
+                                        C_flonum(&a, r->R));
+                       C_return(callback(lst));"))
+
+    ;; returns #(ra dec) 
+    (define mercury-equ-coords
+        (foreign-safe-lambda* scheme-object ((double jd))
+                       "C_word lst = C_SCHEME_END_OF_LIST, *a;
+                       struct ln_equ_posn *r;
+                       r = malloc(sizeof(struct ln_equ_posn));
+                       ln_get_mercury_equ_coords(jd, r);
+                       a = C_alloc(C_SIZEOF_LIST(2) + C_SIZEOF_FLONUM * 2);
+                       lst = C_vector(&a, 2, 
+                                        C_flonum(&a, r->ra),
+                                        C_flonum(&a, r->dec));
+                       C_return(callback(lst));"))
+
+    ;; returns #(ra dec) 
+    (define mercury-equ-coords 
+        (foreign-safe-lambda* scheme-object ((double jd))
+                       "C_word lst = C_SCHEME_END_OF_LIST, *a;
+                       struct ln_equ_posn *r;
+                       r = malloc(sizeof(struct ln_equ_posn));
+                       ln_get_mercury_equ_coords(jd, r);
+                       a = C_alloc(C_SIZEOF_LIST(2) + C_SIZEOF_FLONUM * 2);
+                       lst = C_vector(&a, 2, 
+                                        C_flonum(&a, r->ra),
+                                        C_flonum(&a, r->dec));
+                       C_return(callback(lst));"))
+
+    (define mercury-earth-dist 
+      (foreign-lambda double "ln_get_mercury_earth_dist" double))
+    (define mercury-solar-dist
+      (foreign-lambda double "ln_get_mercury_solar_dist" double))
+    (define mercury-magnitude 
+      (foreign-lambda double "ln_get_mercury_magnitude" double))
+    (define mercury-disk 
+      (foreign-lambda double "ln_get_mercury_disk" double))
+    (define mercury-phase 
+      (foreign-lambda double "ln_get_mercury_phase" double))
+
+    ;; returns #(X Y Z) 
+    (define mercury-rect-helio
+        (foreign-safe-lambda* scheme-object ((double jd))
+                       "C_word lst = C_SCHEME_END_OF_LIST, *a;
+                       struct ln_rect_posn *r;
+                       r = malloc(sizeof(struct ln_rect_posn));
+                       ln_get_mercury_rect_helio(jd, r);
+                       a = C_alloc(C_SIZEOF_LIST(3) + C_SIZEOF_FLONUM * 3);
+                       lst = C_vector(&a, 3, 
+                                        C_flonum(&a, r->X),
+                                        C_flonum(&a, r->Y),
+                                        C_flonum(&a, r->Z));
+                       C_return(callback(lst));"))
+
+ ;;; }}}
+
+
+
+
+)              
+
+
