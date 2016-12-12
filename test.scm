8335d91448f01c3cb6cef13f6e6bd291560e97c5
Robert Altenburg <rca@qrpc.com>
2016-12-11 22:57:58 -0500

    testing

--- test.scm
+++ test.scm
@@ -1,10 +1,10 @@
-(use numbers srfi-19 ephem )
+(use numbers srfi-19 ephem fmt)
 
 ; mayhill lnlat -105.5287 32.903
 ; harrisburg lnlat -76.8867 40.2732 
 ; b33 radec 5.683 -2.4583
 
-(define now (exact->inexact (current-julian-day)))
+(define now (exact->inexact (date->julian-day (current-date (* 3600 -7)))))
 ;(define dd (exact->inexact (date->julian-day (make-date 0 0 0 12 1 1 2000 0 #f))))
 
 ;(define rst (object-rst now -105.5287 32.903 5.683 2.4583)) 
@@ -18,6 +18,9 @@
 ;(define set (get-date (vector-ref rst 1)))
 ;(define transit (get-date (vector-ref rst 2)))
 
-(printf "rise: ~S~%set: ~S~%transit: ~S~%" (rstc-rise rst) (rstc-set rst) (rstc-transit rst))
+(fmt #t 
+     (get-date (rstc-rise rst)) nl
+     (get-date (rstc-set rst)) nl)
+
 
 
